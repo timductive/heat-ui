2e2c7036409239ac4215b1a7aaf7246e4e1876ee
Tim Schnell <tim.schnell@rackspace.com>
2013-06-03 17:18:12 -0500

    working deployments and workflows mockups

--- heat/deployments/views.py
+++ heat/deployments/views.py
@@ -1,5 +1,40 @@
-from django.views import generic
+from datetime import datetime
 
-class IndexView(generic.TemplateView):
-  #  table_class = ThermalCataloguesTable
-     template_name = 'heat/deployments/index.html'
+from django.utils.safestring import mark_safe
+
+from horizon import exceptions
+from horizon import tables
+
+from heat.common.tables import DeploymentsTable
+
+class TableData(object):
+    id = '1'
+    stack_name = 'Wordpress'
+    creation_time = str(datetime.now())
+    updated_time = str(datetime.now())
+    stack_status = 'Create Complete'
+    view = mark_safe("<a href=''>Topology</a> | "
+                              "<a href=''>Logs</a>")
+
+d1 = TableData()
+d2 = TableData()
+d2.id = '2'
+d2.stack_status = 'Create in Progress'
+d2.view = mark_safe(
+        "<a href=''>Topology</a> | "
+        "<a href=''>Workflow</a> | "
+        "<a href=''>Logs</a>"
+    )
+
+class IndexView(tables.DataTableView):
+    table_class = DeploymentsTable
+    template_name = 'heat/deployments/index.html'
+
+    def get_data(self):
+        request = self.request
+        try:
+            stacks = [d1, d2]
+        except:
+            exceptions.handle(request,
+                              ('Unable to retrieve stack list.'))
+        return stacks
--- heat/workflows/views.py
+++ heat/workflows/views.py
@@ -1,5 +1,44 @@
-from django.views import generic
+from datetime import datetime
 
-class IndexView(generic.TemplateView):
-  #  table_class = ThermalCataloguesTable
+from django.utils.safestring import mark_safe
+
+from horizon import exceptions
+from horizon import tables
+
+from heat.common.tables import WorkflowTable
+
+class TableData(object):
+    id = '1'
+    stack_name = 'Wordpress'
+    creation_time = str(datetime.now())
+    updated_time = str(datetime.now())
+    stack_status = 'Create In Progress'
+    view = mark_safe(
+        "<a href=''>Tasks</a> | "
+        "<a href=''>Topology</a> | "
+        "<a href=''>Logs</a>")
+
+d1 = TableData()
+d2 = TableData()
+d2.id = '2'
+d2.stack_status = 'Create Failed'
+d2.view = mark_safe(
+        "<a href=''>Retry</a> | "
+        "<a href=''>Tasks</a> | "
+        "<a href=''>Topology</a> | "
+        "<a href=''>Logs</a>"
+)
+
+
+class IndexView(tables.DataTableView):
+    table_class = WorkflowTable
     template_name = 'heat/workflows/workflows.html'
+
+    def get_data(self):
+        request = self.request
+        try:
+            stacks = [d1, d2]
+        except:
+            exceptions.handle(request,
+                              ('Unable to retrieve stack list.'))
+        return stacks
