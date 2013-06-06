7933d5f1f3defa5bcc324fcf9962d072b2957e2e
Tim Schnell <tim.schnell@rackspace.com>
2013-06-06 13:55:22 -0500

    working mockups

--- heat/logging/views.py
+++
@@ -1,5 +0,0 @@
-from django.views import generic
-
-class IndexView(generic.TemplateView):
-  #  table_class = ThermalCataloguesTable
-    template_name = 'heat/logging/logging.html'
--- heat/logs/views.py
+++ heat/logs/views.py
@@ -1,5 +1,16 @@
+from datetime import datetime
+
 from django.views import generic
 
 class IndexView(generic.TemplateView):
-  #  table_class = ThermalCataloguesTable
     template_name = 'heat/logging/logging.html'
+
+    def get_context_data(self, **kwargs):
+        now = datetime.now()
+        today = now.strftime("%m/%d/%Y")
+
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        context['today'] = today
+
+        return context
